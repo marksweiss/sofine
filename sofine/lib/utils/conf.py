ab2d2660f830fd4d4f15bfa9d6703c4e4af78cba
Mark S. Weiss <marksweiss@yahoo.com>
2014-07-14 03:33:24 -0400

    Fixed whitespace

--- sofine/lib/utils/conf.py
+++ sofine/lib/utils/conf.py
@@ -10,16 +10,16 @@ sys.path.insert(0, PLUGIN_BASE_PATH)
 
 CUSTOM_PLUGIN_BASE_PATH = None
 try:
-	plugin_conf = json.load(open('/'.join(inspect.stack()[0][1].split('/')[:-4]) + '/plugin.conf'))
-	CUSTOM_PLUGIN_BASE_PATH = plugin_conf['plugin_path']
-	sys.path.insert(0, CUSTOM_PLUGIN_BASE_PATH) 
+    plugin_conf = json.load(open('/'.join(inspect.stack()[0][1].split('/')[:-4]) + '/plugin.conf'))
+    CUSTOM_PLUGIN_BASE_PATH = plugin_conf['plugin_path']
+    sys.path.insert(0, CUSTOM_PLUGIN_BASE_PATH) 
 except:
-	pass
+    pass
 
 if not CUSTOM_PLUGIN_BASE_PATH:
-	try:
-		plugin_conf = json.load(open('/'.join(inspect.stack()[0][1].split('/')[:-4]) + '/example.plugin.conf'))
-		CUSTOM_PLUGIN_BASE_PATH = plugin_conf['plugin_path']
-		sys.path.insert(0, CUSTOM_PLUGIN_BASE_PATH) 
-	except:
-		pass
+    try:
+        plugin_conf = json.load(open('/'.join(inspect.stack()[0][1].split('/')[:-4]) + '/example.plugin.conf'))
+        CUSTOM_PLUGIN_BASE_PATH = plugin_conf['plugin_path']
+        sys.path.insert(0, CUSTOM_PLUGIN_BASE_PATH) 
+    except:
+        pass
