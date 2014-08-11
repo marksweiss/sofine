1c595966228bd814f21cbd404bac26dce2ea8e92
Mark S. Weiss <marksweiss@yahoo.com>
2014-08-11 02:45:56 -0400

    Working example app that uses fidelity and ystockqoutelib and writes data collected to Postgresql

--- sofine/examples/portfolio_collector.ddl
+++ sofine/examples/portfolio_collector.ddl
@@ -1,6 +1,22 @@
--- NOTE: Assumes this was done previously
+-- To get Postgres psycopg2 to work on OSX Mavericks
+-- 1) Install Postgres.app
+-- 2) Append path to Postgres.app bin directory to end of $PATH in .bash_profile
+-- 3) At this point running py scripts that include psycopg2 will work, but import
+--    in the Python shell will not, so ...
+-- 4) $ sudo ln -s /Applications/Postgres.app/Contents/Versions/9.3/lib/libssl.1.0.0.dylib /opt/local/lib/libssl.1.0.0.dylib
+-- 5) $ sudo ln -s /Applications/Postgres.app/Contents/Versions/9.3/lib/libcrypto.1.0.0.dylib /opt/local/lib/libcrypto.1.0.0.dylib
+-- (Anaconda) Psycopg2 in Python shell can't find and link to these dylibs that 
+-- are part of Postgres distro unless there sare sym links in the fs under the same user
+-- that launches the Python shell
+-- 6) Use the default data directory
+-- 7) createdb -h localhost
+-- This gets past error where launching the db gives error 
+--  psql: FATAL:  database "<USER>" does not exist
+
+-- NOTE: Assumes these steps were done previously
+-- CLI: initdb -D /Users/markweiss/Library/Application\ Support/Postgres/var-9.3 -E UTF8
 -- template0 thing necessary to override default encoding of SQL_ASCII with Postgres.app
--- create database sofine_portfolio with template = template0 owner=postgres encoding='UTF8';
+-- CLI: createdb -E UTF8 sofine_portfolio
 
 \connect sofine_portfolio;
 
--- sofine/examples/portfolio_collector.py
+++ sofine/examples/portfolio_collector.py
@@ -1,16 +1,90 @@
 # TODO
-# - write script to collect data from fidelity and ystockquote
-# - save data into postgresql hstore k/v store
 # - write code to perform useful queries with business rules
 # - wrapper to update the data and report
 # - cron somewhere
 
-# TODO ADD TO README for this example
-# install Posgress.app
-# pip install psycopg2
 
-# Before running Python script that imports psycopg2
-# Do NOT do this in .bash_profile, as if affects any other process trying to link to
-#  any other lib. So just precede call to run Py scripts using pscyopg2 with this
-# export DYLD_LIBRARY_PATH=/Applications/Postgres.app/Contents/Versions/9.3/lib
+def _get_data(c, customer_id, p, password, a, account_id, e, email):
+    import sofine.runner as runner
+    import psycopg2
+    
+    # sofine pipeline to collect the data
+    data = {}
+    plugins = ['fidelity', 'ystockquotelib']
+    plugin_groups = ['example', 'example']
+    fidelity_args = [c, customer_id, p, password, a, account_id, e, email]
+    plugin_args = [fidelity_args, []]
+    
+    data = runner.get_data_batch(data, plugins, plugin_groups, plugin_args)
+    
+    return data
+
+
+def _store_data(insert_data, db_password):
+    import psycopg2
+
+    
+    def report_db_error(e):
+        print('Error code: {0}, Error: {1}, Dianostic severity: {2}, Diagnostic message: {3}'.format(
+            e.pgcode, e.pgerror, e.diag.severity, e.diag.message_primary))
+
+    
+    db_name = 'sofine_portfolio'
+    db_user = 'markweiss'
+    
+    conn = None
+    conn_str = ''
+    try:
+        conn_str = "dbname='{0}' user='{1}' password='{2}'".format(db_name, db_user, db_password)
+        conn = psycopg2.connect(conn_str)
+    except Exception as e:
+        print('Failed to connect. Connection string: {0}'.format(conn_str))
+        report_db_error(e)
+        return
+
+    cur = conn.cursor()
+    # Create a list of tuples to create a bulk insert statement
+    insert_data_str = ','.join(cur.mogrify('%s', (x, )) for x in insert_data)
+    stmt = """INSERT INTO portfolio_data (key, attr_key, attr_value) 
+              VALUES {0};""".format(insert_data_str)
+    try:
+        cur.execute(stmt)
+        conn.commit()
+    except Exception as e:
+        print('Failed to execute or failed to commit. Statement being executed: {0}'.format(stmt))
+        report_db_error(e)
+        return
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+
+def main(c, customer_id, p, password, a, account_id, e, email, db_password):
+    data = _get_data(c, customer_id, p, password, a, account_id, e, email)
+
+    # Flatten into the structure needed to bulk insert into postgres with psycopg2
+    insert_data = []
+    for key in data.keys():
+        for attr_key in data[key]:
+            insert_data.append( (str(key), str(attr_key), str(data[key][attr_key])) )
+   
+    _store_data(insert_data, db_password)
+
+
+# Sample call: PROJECT_ROOT$ python ./tests/test_runner_from_cli_examples.py \
+#              -c MY_CUSTOMER_ID \
+#              -p MY_PASSWORD \
+#              -a MY_ACCOUNT_ID \
+#              -e MY_EMAIL \
+#              -d DB PASSWORD
+if __name__ == '__main__':
+    import sys
+
+    # Load the module scope variables from CLI args
+    # This lets us not hard-code sensitive values needed for the test
+    c, customer_id, p, password, a, account_id, e, email, d, db_password = sys.argv[1:]
+    
+    main(c, customer_id, p, password, a, account_id, e, email, db_password)
 
