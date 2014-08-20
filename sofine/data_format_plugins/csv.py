b9a15bc010e9def1306631eee9816b02a3365e75
Mark S. Weiss <marksweiss@yahoo.com>
2014-08-20 19:15:35 -0400

    Intermi commit. Added support for csv properties to csv plugin. Cleaned up serialize and deserialize to properly use csv module and csv properties. Settled on data format with no header row of field names.

--- sofine/data_format_plugins/csv.py
+++ sofine/data_format_plugins/csv.py
@@ -1,53 +1,83 @@
 import csv
+import cStringIO
 import sys
+
+# TODO Document only supported CSV format
+# - No column headers
+# - Key is always position 0
+# - Attr keys and values alternate in positions 1..n
+
+
+delimiter = ','
+def set_delimiter(c):
+    delimiter = c
+
+
+lineterminator = '\n'
+def set_line_terminator(s):
+    lineterminator = s
+
+
+quoting = csv.QUOTE_NONE
+def set_quoating_none():
+    quoting_policy = csv.QUOTE_NONE
+
+def set_quoting_all():
+    quoting_policy = csv.QUOTE_ALL
+
+def set_quoting_minimal():
+    quoting_policy = csv.MINIMAL
+
+def set_quoting_nonnumeric():
+    quoting_policy = csv.NONNUMERIC
+
+
+quotechar = '"'
+def set_quote_char(c):
+    quotechar = c
 
 
 def deserialize(data):
-    data = {}
+    ret = {}
     schema = []
+  
+    in_strm = cStringIO.StringIO(data) 
+    reader = csv.reader(in_strm, encoding="utf-8", delimiter=delimiter,
+                        lineterminator=lineterminator, quoting=quoting, quotechar=quotechar)
 
-    idx = 0
-    reader = csv.reader(sys.stdin)
     for row in reader:
-        # TODO Figure out whether we can support a format without column names
-        # TODO This conditional sucks
-        if idx == 0:
-            # TODO Support configuring csv delimiter
-            schema = row.split(',')
-        else:
-            # 0th elem in CSV row is data row key
-            key = row[0]
-            data[key] = []
-            # Loop over the schema and attr values in the rest of CSV row
-            #  and build dicts an put in array of attrs for this key
-            for attr_idx in range(1, len(row)):
-                schema_idx = attr_idx - 1
-                data[key].append({schema[schema_idx] : row[attr_idx]})
+        # 0th elem in CSV row is data row key
+        key = row[0]
+        # Remaining data in CSV row are the attribute key/vals. Convert to array of key/val dicts
+        ret[key] = [{row[j] : row[j + 1]} for j in range(1, len(row) - 1)]
+   
+    in_data_strm.close()
     
-    return data
+    return ret
 
 
 def serialize(data):
-    # TODO Support quote-qualifying each value
-    # csv.QUOTE_ALL, QUOTE_MINIMAL, QUOTE_NONNUMERIC, QUOTE_NONE
-    # TODO Look at the Dialect object, many other settings around espcaping, etc.
-    # Support this by exposing a config interface similar to file_source
-    rows = []
+    out_strm = cStringIO.StringIO()
+    writer = csv.writer(out_strm, encoding="utf-8", delimiter=delimiter,
+                        lineterminator=lineterminator, quoting=quoting, quotechar=quotechar)
+    
     # Flatten each key -> [attrs] 'row' in data into a CSV row with
     #  key in the 0th position, and the attr values in an array in fields 1 .. N
     for key, attrs in data.iteritems():
         row = []
         row.append(str(key))
         for attr in attrs:
-            # TODO Support column headers on first line with call to get_schema()
-            # TODO Figure out whether we can support something without column headers
-            # row.append(str(attr.keys()[0]))
+            row.append(str(attr.keys()[0]))
             row.append(str(attr.values()[0]))
-        rows.append(row)
-    
-    # TODO Support configuring csv delimiter
-    return ','.join(rows)
+        writer.writerow(row)
+
+    ret = out_strm.getvalue()
+    out_strm.close()
+
+    return ret
 
 
 def get_content_type():
-    return 'application/csv'
+    return 'text/csv'
+
+
