0a9091c0cf4ace241c270e6e4bc446e804e669fc
Mark S. Weiss <marksweiss@yahoo.com>
2014-09-17 01:37:03 -0400

    Ruby plugin passing manual tests for all four actions

--- sofine/plugins/example_http/google_search_results.rb
+++ sofine/plugins/example_http/google_search_results.rb
@@ -1,41 +1,51 @@
 require 'net/http'
 require 'open-uri'
+require 'json'
 require 'sinatra'
-set :server, 'thin'
 
 
-def query_google_search(k):
-    """
+def query_google_search(k) 
+'
 * `k` - `string`. The query term.
 
 Helper that calls Google Search API with a query and returns JSON results set. 
-Returns an array of JSON objects in the `['responseData']['results']` value  as 
+Returns an array of JSON objects in the `["responseData"]["results"]` value  as 
 described in the documentation for `get_child_schema`.
-"""    
+'    
   k = URI::encode(k)
-  url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=#{k}'
+  url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=#{k}"
   ret = Net::HTTP.get_response(URI(url)) 
-  ret = ret.body
+  ret = JSON.load(ret.body)
     
-  if ret: 
-    ret = {'results' : ret['responseData']['results']}
-  else:
-    ret = {'results' : []}
+  if ret 
+    ret = {'results' => ret['responseData']['results']}
+  else
+    ret = {'results' => []}
+  end
   
   ret
-
+end
 
 
 get '/parse_args' do
-  args = params['args']
-  '{"parsed_args" : "#{args}", "is_valid" : true}'   
+  JSON.dump({"parsed_args" => params[args], "is_valid" => true})
 end
 
 
 get '/get_data' do
-  keys = params['keys']
-  args = params['args']
+  keys = params['keys'].split(',')
   
+  # TEMP DEBUG
+  # query_google_search(keys[0])
+
+  ret = {}
+  keys.each {|key| 
+    attrs = []
+    attrs.insert(0, query_google_search(key))
+    ret[key] = attrs 
+  }
+
+  JSON.dump(ret)
 end
 
 
@@ -47,7 +57,4 @@ end
 get '/adds_keys' do
   '{"adds_keys" : false}'
 end
-
-
-run Sinatra::Application
 
